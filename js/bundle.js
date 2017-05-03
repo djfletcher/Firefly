71614df97c44c7dbdd7f39af64ab348a50e7e9e7
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-02 19:24:55 -0700

    Add SF parks and contoured terain layers

--- js/bundle.js
+++ js/bundle.js
@@ -83,7 +83,45 @@ window.addEventListener("DOMContentLoaded", function () {
   _mapboxGl2.default.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
   var map = new _mapboxGl2.default.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v9'
+    style: 'mapbox://styles/mapbox/dark-v9',
+    center: [-77.186587, 38.895939],
+    zoom: 5,
+    scrollZoom: false
+  });
+
+  map.on('load', function () {
+    map.addLayer({
+      id: 'terrain-data',
+      type: 'line',
+      source: {
+        type: 'vector',
+        url: 'mapbox://mapbox.mapbox-terrain-v2'
+      },
+      'source-layer': 'contour',
+      "paint": {
+        "line-color": "#32cd32"
+      }
+    });
+
+    map.addLayer({
+      id: 'rpd_parks',
+      type: 'fill',
+      source: {
+        type: 'vector',
+        url: 'mapbox://mapbox.3o7ubwm8'
+      },
+      'source-layer': 'RPD_Parks',
+      "paint": {
+        "fill-color": "#32cd32"
+      }
+    });
+  });
+
+  map.on('click', function () {
+    map.flyTo({
+      center: [-77.186587, 38.895939],
+      zoom: 17
+    });
   });
 });
 
