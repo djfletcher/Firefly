b375639eb76a7c24460a33baf7612fd13414d4d3
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-05 18:37:57 -0700

    Successfully fetch airport geometry from geocoding api

--- js/firefly.js
+++ js/firefly.js
@@ -1,4 +1,47 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
+import { domestic } from './routes';
+
+let airports = [];
+
+const fetchCoords = airport => {
+  let airportId = airport.id;
+  $.ajax({
+    method: 'GET',
+    url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${airportId}.json?`
+          + `access_token=pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g`
+          + `&types=poi&country=us`,
+    success: function(r) {
+      airport['geometry'] = r.features[0].geometry;
+      airports.push(airport);
+    }
+  });
+};
+
+[
+  {
+    'id': 'ABQ',
+    'name':	'Albuquerque, NM'
+  },
+  {
+    'id': 'ACV',
+    'name':	'Eureka, CA'
+  },
+  {
+    'id': 'ANC',
+    'name':	'Anchorage, AK'
+  }
+].forEach(a => fetchCoords(a));
+
+// const airports = domestic.map(a => {
+//   let geometry = fetchCoords(a.id);
+//   a['geometry'] = geometry;
+//   return a;
+// });
+
+console.log(airports);
+
+window.fetchCoords = fetchCoords;
+window.airports = airports;
 
 window.addEventListener("DOMContentLoaded", () => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
--- js/routes.js
+++ js/routes.js
@@ -1,138 +1,375 @@
 // Source: http://www.flysfo.com/media/facts-statistics/north-america-routes
-export const domestic = {
-  ABQ	Albuquerque, NM
-  ACV	Eureka, CA
-  ANC	Anchorage, AK
-  ASE	Aspen-Pitkin, CO
-  ATL	Atlanta, GA
-  AUS	Austin, TX
-  BFL	Bakersfield, CA
-  BNA	Nashville, TN
-  BOI	Boise, ID
-  BOS	Boston, MA
-  BUR	Burbank, CA
-  BWI	Baltimore, MD
-  BZN	Bozeman, MT
-  CLE	Cleveland, OH
-  CLT	Charlotte, NC
-  CVG	Cincinnati, OH
-  DAL	Dallas Love, TX
-  DCA	Washington-Reagan, VA
-  DEN	Denver, CO
-  DFW	Dallas/Fort Worth, TX
-  DTW	Detroit, MI
-  EUG	Eugene, OR
-  EWR	Newark, NJ
-  FAT	Fresno, CA
-  FLL	Fort Lauderdale, FL
-  HDN	Hayden/Yampa Valley, CO
-  HNL	Honolulu/Oahu, HI
-  IAD	Washington-Dulles, VA
-  IAH 	Houston, TX
-  IND	Indianapolis, IN
-  JAC	Jackson Hole, WY
-  JFK	New York, NY
-  KOA 	Kona-Hawaii, HI
-  LAS	Las Vegas, NV
-  LAX	Los Angeles, CA
-  LGB	Long Beach, CA
-  LIH	Lihue/Kauai, HI
-  MCI	Kansas City, MO
-  MCO	Orlando, FL
-  MDW	Chicago-Midway, IL
-  MFR	Medford, OR
-  MIA	Miami, FL
-  MKE	Milwaukee, WI
-  MMH	Mammoth Lakes, CA
-  MRY	Monterey, CA
-  MSO	Missoula, MT
-  MSP	Minneapolis/St. Paul, MN
-  MSY	New Orleans, LA
-  MTJ	Montrose, CO
-  OGG	Kahului/Maui, HI
-  OKC	Oklahoma City, OK
-  ONT	Ontario, CA
-  ORD	Chicago-O'Hare, IL
-  OMA	Omaha, NE
-  OTH 	North Bend, OR
-  PDX	Portland, OR
-  PHL	Philadelphia, PA
-  PHX	Phoenix, AZ
-  PIT	Pittsburgh, PA
-  PSC	Pasco/Tri-Cities, WA
-  PSP	Palm Springs, CA
-  RDD	Redding, CA
-  RDM	Redmond, OR
-  RDU	Raleigh-Durham, NC
-  RNO	Reno, NV
-  SAN	San Diego, CA
-  SAT	San Antonio, TX
-  SBA	Santa Barbara, CA
-  SBP	San Luis Obispo, CA
-  SEA	Seattle/Tacoma, WA
-  SJD	San Jose Cabo, MEX
-  SLC	Salt Lake City, UT
-  SMF	Sacramento, CA
-  SMX	Santa Maria, CA
-  SNA	Orange County, CA
-  STL	St. Louis, MO
-  SUN	Sun Valley, ID
-  TUS	Tucson, AZ
-  XNA	Northwest Arkansas/Fayetteville, AR
-};
+export const domestic = [
+  {
+    'id': 'ABQ',
+    'name':	'Albuquerque, NM'
+  },
+  {
+    'id': 'ACV',
+    'name':	'Eureka, CA'
+  },
+  {
+    'id': 'ANC',
+    'name':	'Anchorage, AK'
+  },
+  {
+    'id': 'ASE',
+    'name':	'Aspen-Pitkin, CO'
+  },
+  {
+    'id': 'ATL',
+    'name':	'Atlanta, GA'
+  },
+  {
+    'id': 'AUS',
+    'name':	'Austin, TX'
+  },
+  {
+    'id': 'BFL',
+    'name':	'Bakersfield, CA'
+  },
+  {
+    'id': 'BNA',
+    'name':	'Nashville, TN'
+  },
+  {
+    'id': 'BOI',
+    'name':	'Boise, ID'
+  },
+  {
+    'id': 'BOS',
+    'name':	'Boston, MA'
+  },
+  {
+    'id': 'BUR',
+    'name':	'Burbank, CA'
+  },
+  {
+    'id': 'BWI',
+    'name':	'Baltimore, MD'
+  },
+  {
+    'id': 'BZN',
+    'name':	'Bozeman, MT'
+  },
+  {
+    'id': 'CLE',
+    'name':	'Cleveland, OH'
+  },
+  {
+    'id': 'CLT',
+    'name':	'Charlotte, NC'
+  },
+  {
+    'id': 'CVG',
+    'name':	'Cincinnati, OH'
+  },
+  {
+  'id': '  DAL',
+  'name': 'Dallas Love, TX'
+  },
+  {
+    'id': 'DCA',
+    'name': "Washington-Reagan, VA"
+  },
+  {
+    'id': 'DEN',
+    'name': "Denver, CO"
+  },
+  {
+    'id': 'DFW',
+    'name': "Dallas/Fort Worth, TX"
+  },
+  {
+    'id': 'DTW',
+    'name': "Detroit, MI"
+  },
+  {
+    'id': 'EUG',
+    'name': "Eugene, OR"
+  },
+  {
+    'id': 'EWR',
+    'name': "Newark, NJ"
+  },
+  {
+    'id': 'FAT',
+    'name': "Fresno, CA"
+  },
+  {
+    'id': 'FLL',
+    'name': "Fort Lauderdale, FL"
+  },
+  {
+    'id': 'HDN',
+    'name': "Hayden/Yampa Valley, CO"
+  },
+  {
+    'id': 'HNL',
+    'name': "Honolulu/Oahu, HI"
+  },
+  {
+    'id': 'IAD',
+    'name': "Washington-Dulles, VA"
+  },
+  {
+    'id': 'IAH',
+    'name': "Houston, TX"
+  },
+  {
+    'id': 'IND',
+    'name': "Indianapolis, IN"
+  },
+  {
+    'id': 'JAC',
+    'name': "Jackson Hole, WY"
+  },
+  {
+    'id': 'JFK',
+    'name': "New York, NY"
+  },
+  {
+    'id': 'KOA',
+    'name': "Kona-Hawaii, HI"
+  },
+  {
+    'id': 'LAS',
+    'name': "Las Vegas, NV"
+  },
+  {
+    'id': 'LAX',
+    'name': "Los Angeles, CA"
+  },
+  {
+    'id': 'LGB',
+    'name': "Long Beach, CA"
+  },
+  {
+    'id': 'LIH',
+    'name': "Lihue/Kauai, HI"
+  },
+  {
+    'id': 'MCI',
+    'name': "Kansas City, MO"
+  },
+  {
+    'id': 'MCO',
+    'name': "Orlando, FL"
+  },
+  {
+    'id': 'MDW',
+    'name': "Chicago-Midway, IL"
+  },
+  {
+    'id': 'MFR',
+    'name': "Medford, OR"
+  },
+  {
+    'id': 'MIA',
+    'name': "Miami, FL"
+  },
+  {
+    'id': 'MKE',
+    'name': "Milwaukee, WI"
+  },
+  {
+    'id': 'MMH',
+    'name': "Mammoth Lakes, CA"
+  },
+  {
+    'id': 'MRY',
+    'name': "Monterey, CA"
+  },
+  {
+    'id': 'MSO',
+    'name': "Missoula, MT"
+  },
+  {
+    'id': 'MSP',
+    'name': "Minneapolis/St. Paul, MN"
+  },
+  {
+    'id': 'MSY',
+    'name': "New Orleans, LA"
+  },
+  {
+    'id': 'MTJ',
+    'name': "Montrose, CO"
+  },
+  {
+    'id': 'OGG',
+    'name': "Kahului/Maui, HI"
+  },
+  {
+    'id': 'OKC',
+    'name': "Oklahoma City, OK"
+  },
+  {
+    'id': 'ONT',
+    'name': "Ontario, CA"
+  },
+  {
+    'id': 'ORD',
+    'name': "Chicago-O'Hare, IL"
+  },
+  {
+    'id': 'OMA',
+    'name': "Omaha, NE"
+  },
+  {
+    'id': 'OTH',
+    'name': "	North Bend, OR"
+  },
+  {
+    'id': 'PDX',
+    'name': "Portland, OR"
+  },
+  {
+    'id': 'PHL',
+    'name': "Philadelphia, PA"
+  },
+  {
+    'id': 'PHX',
+    'name': "Phoenix, AZ"
+  },
+  {
+    'id': 'PIT',
+    'name': "Pittsburgh, PA"
+  },
+  {
+    'id': 'PSC',
+    'name': "Pasco/Tri-Cities, WA"
+  },
+  {
+    'id': 'PSP',
+    'name': "Palm Springs, CA"
+  },
+  {
+    'id': 'RDD',
+    'name': "Redding, CA"
+  },
+  {
+    'id': 'RDM',
+    'name': "Redmond, OR"
+  },
+  {
+    'id': 'RDU',
+    'name': "Raleigh-Durham, NC"
+  },
+  {
+    'id': 'RNO',
+    'name': "Reno, NV"
+  },
+  {
+    'id': 'SAN',
+    'name': "San Diego, CA"
+  },
+  {
+    'id': 'SAT',
+    'name': "San Antonio, TX"
+  },
+  {
+    'id': 'SBA',
+    'name': "Santa Barbara, CA"
+  },
+  {
+    'id': 'SBP',
+    'name': "San Luis Obispo, CA"
+  },
+  {
+    'id': 'SEA',
+    'name': "Seattle/Tacoma, WA"
+  },
+  {
+    'id': 'SJD',
+    'name': "San Jose Cabo, MEX"
+  },
+  {
+    'id': 'SLC',
+    'name': "Salt Lake City, UT"
+  },
+  {
+    'id': 'SMF',
+    'name': "Sacramento, CA"
+  },
+  {
+    'id': 'SMX',
+    'name': "Santa Maria, CA"
+  },
+  {
+    'id': 'SNA',
+    'name': "Orange County, CA"
+  },
+  {
+    'id': 'STL',
+    'name': "St. Louis, MO"
+  },
+  {
+    'id': 'SUN',
+    'name': "Sun Valley, ID"
+  },
+  {
+    'id': 'TUS',
+    'name': "Tucson, AZ"
+  },
+  {
+    'id': 'XNA',
+    'name': "Northwest Arkansas/Fayetteville, AR"
+  }
+];
 
 // Source: https://www.flysfo.com/media/facts-statistics/international-routes
-export const international = {
-  AKL	Auckland, NZ
-  AMS	Amsterdam, NL
-  AUH	Abu Dhabi, AE
-  BLR	Bangalore, IN (via New Dehli)
-  CAN	Guangzhou, CN
-  CDG	Paris-De Gaulle, FR
-  CKG	Chongqing, CN (via Beijing)
-  CPH	Copenhagen, DK
-  CTU	Chengdu, CN
-  CUN	Cancun, MX
-  DEL	Delhi, IN
-  DUB	Dublin, IE
-  DUS	Dusseldorf, DE
-  DXB	Dubai, AE
-  FRA	Frankfurt, DE
-  GDL	Guadalajara, MX
-  HEL	Helsinki, FI
-  HGH	Hangzhou, CN
-  HKG	Hong Kong, HK
-  HND	Tokyo-Haneda, JP
-  ICN	Seoul-Incheon, KR
-  IST	Istanbul, TR
-  KEF	Keflavik, IS
-  KIX	Osaka-Kansai, JP
-  LHR	London-Heathrow, EN, GB
-  MAN	Manchester, EN, GB
-  MEX	Mexico City, MX
-  MNL	Manila, PH
-  MUC	Munich, DE
-  NAN	Nadi, FJ
-  NRT	Tokyo-Narita, JP
-  PEK	Beijing, CN
-  PTY	Panama City, PA
-  PVG	Shanghai, CN
-  PVR	Puerto Vallarta, MX
-  SAL	San Salvador, SV
-  SIN	Singapore, SG
-  SJD	San Jose Cabo, MX
-  SYD	Sydney, NS, AU
-  TAO	Qingdao, CN
-  TLV	Tel Aviv, IL
-  TPE	Taipei, TW
-  TXL	Berlin, DE
-  WUH	Wuhan, CN
-  XIY	Xi'an, CN
-  YEG	Edmonton, AB, CA
-  YUL	Montreal-PET, QC, CA
-  YVR	Vancouver, BC, CA
-  YYC	Calgary, AB, CA
-  YYJ	Victoria, BC, CA
-  YYZ	Toronto, ON, CA
-  ZRH	Zurich, CH
-};
+// export const international = {
+//   AKL	Auckland, NZ
+//   AMS	Amsterdam, NL
+//   AUH	Abu Dhabi, AE
+//   BLR	Bangalore, IN (via New Dehli)
+//   CAN	Guangzhou, CN
+//   CDG	Paris-De Gaulle, FR
+//   CKG	Chongqing, CN (via Beijing)
+//   CPH	Copenhagen, DK
+//   CTU	Chengdu, CN
+//   CUN	Cancun, MX
+//   DEL	Delhi, IN
+//   DUB	Dublin, IE
+//   DUS	Dusseldorf, DE
+//   DXB	Dubai, AE
+//   FRA	Frankfurt, DE
+//   GDL	Guadalajara, MX
+//   HEL	Helsinki, FI
+//   HGH	Hangzhou, CN
+//   HKG	Hong Kong, HK
+//   HND	Tokyo-Haneda, JP
+//   ICN	Seoul-Incheon, KR
+//   IST	Istanbul, TR
+//   KEF	Keflavik, IS
+//   KIX	Osaka-Kansai, JP
+//   LHR	London-Heathrow, EN, GB
+//   MAN	Manchester, EN, GB
+//   MEX	Mexico City, MX
+//   MNL	Manila, PH
+//   MUC	Munich, DE
+//   NAN	Nadi, FJ
+//   NRT	Tokyo-Narita, JP
+//   PEK	Beijing, CN
+//   PTY	Panama City, PA
+//   PVG	Shanghai, CN
+//   PVR	Puerto Vallarta, MX
+//   SAL	San Salvador, SV
+//   SIN	Singapore, SG
+//   SJD	San Jose Cabo, MX
+//   SYD	Sydney, NS, AU
+//   TAO	Qingdao, CN
+//   TLV	Tel Aviv, IL
+//   TPE	Taipei, TW
+//   TXL	Berlin, DE
+//   WUH	Wuhan, CN
+//   XIY	Xi'an, CN
+//   YEG	Edmonton, AB, CA
+//   YUL	Montreal-PET, QC, CA
+//   YVR	Vancouver, BC, CA
+//   YYC	Calgary, AB, CA
+//   YYJ	Victoria, BC, CA
+//   YYZ	Toronto, ON, CA
+//   ZRH	Zurich, CH
+// };
