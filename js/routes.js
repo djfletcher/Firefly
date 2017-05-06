e4bade62aad22d7dcf10ef5b0de1b62e39873037
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-05 17:27:13 -0700

    Add SFO route data

---
+++ js/routes.js
@@ -0,0 +1,138 @@
+// Source: http://www.flysfo.com/media/facts-statistics/north-america-routes
+export const domestic = {
+  ABQ	Albuquerque, NM
+  ACV	Eureka, CA
+  ANC	Anchorage, AK
+  ASE	Aspen-Pitkin, CO
+  ATL	Atlanta, GA
+  AUS	Austin, TX
+  BFL	Bakersfield, CA
+  BNA	Nashville, TN
+  BOI	Boise, ID
+  BOS	Boston, MA
+  BUR	Burbank, CA
+  BWI	Baltimore, MD
+  BZN	Bozeman, MT
+  CLE	Cleveland, OH
+  CLT	Charlotte, NC
+  CVG	Cincinnati, OH
+  DAL	Dallas Love, TX
+  DCA	Washington-Reagan, VA
+  DEN	Denver, CO
+  DFW	Dallas/Fort Worth, TX
+  DTW	Detroit, MI
+  EUG	Eugene, OR
+  EWR	Newark, NJ
+  FAT	Fresno, CA
+  FLL	Fort Lauderdale, FL
+  HDN	Hayden/Yampa Valley, CO
+  HNL	Honolulu/Oahu, HI
+  IAD	Washington-Dulles, VA
+  IAH 	Houston, TX
+  IND	Indianapolis, IN
+  JAC	Jackson Hole, WY
+  JFK	New York, NY
+  KOA 	Kona-Hawaii, HI
+  LAS	Las Vegas, NV
+  LAX	Los Angeles, CA
+  LGB	Long Beach, CA
+  LIH	Lihue/Kauai, HI
+  MCI	Kansas City, MO
+  MCO	Orlando, FL
+  MDW	Chicago-Midway, IL
+  MFR	Medford, OR
+  MIA	Miami, FL
+  MKE	Milwaukee, WI
+  MMH	Mammoth Lakes, CA
+  MRY	Monterey, CA
+  MSO	Missoula, MT
+  MSP	Minneapolis/St. Paul, MN
+  MSY	New Orleans, LA
+  MTJ	Montrose, CO
+  OGG	Kahului/Maui, HI
+  OKC	Oklahoma City, OK
+  ONT	Ontario, CA
+  ORD	Chicago-O'Hare, IL
+  OMA	Omaha, NE
+  OTH 	North Bend, OR
+  PDX	Portland, OR
+  PHL	Philadelphia, PA
+  PHX	Phoenix, AZ
+  PIT	Pittsburgh, PA
+  PSC	Pasco/Tri-Cities, WA
+  PSP	Palm Springs, CA
+  RDD	Redding, CA
+  RDM	Redmond, OR
+  RDU	Raleigh-Durham, NC
+  RNO	Reno, NV
+  SAN	San Diego, CA
+  SAT	San Antonio, TX
+  SBA	Santa Barbara, CA
+  SBP	San Luis Obispo, CA
+  SEA	Seattle/Tacoma, WA
+  SJD	San Jose Cabo, MEX
+  SLC	Salt Lake City, UT
+  SMF	Sacramento, CA
+  SMX	Santa Maria, CA
+  SNA	Orange County, CA
+  STL	St. Louis, MO
+  SUN	Sun Valley, ID
+  TUS	Tucson, AZ
+  XNA	Northwest Arkansas/Fayetteville, AR
+};
+
+// Source: https://www.flysfo.com/media/facts-statistics/international-routes
+export const international = {
+  AKL	Auckland, NZ
+  AMS	Amsterdam, NL
+  AUH	Abu Dhabi, AE
+  BLR	Bangalore, IN (via New Dehli)
+  CAN	Guangzhou, CN
+  CDG	Paris-De Gaulle, FR
+  CKG	Chongqing, CN (via Beijing)
+  CPH	Copenhagen, DK
+  CTU	Chengdu, CN
+  CUN	Cancun, MX
+  DEL	Delhi, IN
+  DUB	Dublin, IE
+  DUS	Dusseldorf, DE
+  DXB	Dubai, AE
+  FRA	Frankfurt, DE
+  GDL	Guadalajara, MX
+  HEL	Helsinki, FI
+  HGH	Hangzhou, CN
+  HKG	Hong Kong, HK
+  HND	Tokyo-Haneda, JP
+  ICN	Seoul-Incheon, KR
+  IST	Istanbul, TR
+  KEF	Keflavik, IS
+  KIX	Osaka-Kansai, JP
+  LHR	London-Heathrow, EN, GB
+  MAN	Manchester, EN, GB
+  MEX	Mexico City, MX
+  MNL	Manila, PH
+  MUC	Munich, DE
+  NAN	Nadi, FJ
+  NRT	Tokyo-Narita, JP
+  PEK	Beijing, CN
+  PTY	Panama City, PA
+  PVG	Shanghai, CN
+  PVR	Puerto Vallarta, MX
+  SAL	San Salvador, SV
+  SIN	Singapore, SG
+  SJD	San Jose Cabo, MX
+  SYD	Sydney, NS, AU
+  TAO	Qingdao, CN
+  TLV	Tel Aviv, IL
+  TPE	Taipei, TW
+  TXL	Berlin, DE
+  WUH	Wuhan, CN
+  XIY	Xi'an, CN
+  YEG	Edmonton, AB, CA
+  YUL	Montreal-PET, QC, CA
+  YVR	Vancouver, BC, CA
+  YYC	Calgary, AB, CA
+  YYJ	Victoria, BC, CA
+  YYZ	Toronto, ON, CA
+  ZRH	Zurich, CH
+};
